2154874e683f786b53f086b67304d5cf25e780a6
isaaclo123 <isaaclo123@gmail.com>
2024-11-26 13:35:18 -0600

    lock on suspend

--- home-manager/hyprland/wlogout/default.nix
+++ home-manager/hyprland/wlogout/default.nix
@@ -30,7 +30,7 @@
     }
     {
         label = "suspend";
-        action = "systemctl suspend";
+        action = "systemctl suspend; hyprlock";
         text = "Suspend";
         keybind = "u";
     }
